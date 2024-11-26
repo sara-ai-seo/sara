9adff99d03208823da448173ed5645cd08a2c9b3
josephDev123 <josephuzuegbu55@gmail.com>
2024-11-26 07:25:52 +0100

    created the privacy, term of service page

---
+++ src/app/(staticpages)/privacy-policy/page.tsx
@@ -0,0 +1,229 @@
+export default function page() {
+  return (
+    <section className="flex flex-col h-full w-full">
+      <div className="flex flex-col  justify-center items-center py-16 space-y-3">
+        <h1 className="text-[#1570EF] font-medium">Privacy Policy</h1>
+        <h2 className="font-bold sm:text-4xl text-3xl text-wrap text-center">
+          We care about your privacy
+        </h2>
+        <p className="text-base text-[#475467] max-w-[700px] text-center">
+          Your privacy is important to us at Webmaxi. We respect your privacy
+          regarding any information we may collect from you across our website.
+        </p>
+      </div>
+      <div className="flex flex-col p-4 md:max-w-[720px] mx-auto">
+        <div className="flex flex-col  space-y-4">
+          <h1 className="text-2xl font-medium">Introduction</h1>
+          <p className="text-[#475467]">
+            This Privacy Policy explains how WebMaxi collects, uses, stores,
+            discloses and otherwise processes the personal data we collect when
+            you use and interact with our website (webmaxi.net) and services
+            that display or link to this Privacy Policy, register for an account
+            and use our Services, visit our branded social media pages, register
+            for, attend or take part in our events, webinars, or contests, or
+            otherwise interact with us in person and/or by email, text message,
+            instant messaging, telephonically, and by other electronic means. It
+            also tells you about your rights and choices with respect to your
+            personal data, and how you can contact us if you have any questions
+            or concerns.
+          </p>
+
+          <p className="text-[#475467]">
+            This Privacy Policy does not apply to our handling of personal data
+            that we process on behalf of our business customers as a service
+            provider or processor. Our processing of that information is
+            governed by our agreement with our business customers.
+          </p>
+
+          <p className="text-[#475467]">
+            Please read this Privacy Policy carefully. If you do not agree with
+            the terms of this Privacy Policy, you should not use our website and
+            the Services or otherwise provide us with your personal data. By
+            using and/or accessing our website and the Services or otherwise
+            providing us with your personal data, you acknowledge that you have
+            read and understood this Privacy Policy. Your use of our website and
+            the Services is also governed by our Terms of Service.
+          </p>
+
+          <p className="text-[#475467]">
+            Please read this Privacy Policy carefully. If you do not agree with
+            the terms of this Privacy Policy, you should not use our website and
+            the Services or otherwise provide us with your personal data. By
+            using and/or accessing our website and the Services or otherwise
+            providing us with your personal data, you acknowledge that you have
+            read and understood this Privacy Policy. Your use of our website and
+            the Services is also governed by our Terms of Service.
+          </p>
+        </div>
+
+        <div className="flex flex-col  space-y-4 mt-6">
+          <h1 className="text-2xl font-medium">Key Definitions</h1>
+          <p className="text-[#475467]">
+            "WebMaxi", "we", "us" and/or "our" refers to WebMaxi and its
+            affiliates. "Services" refers to our Software as a Service
+            professional service made available via our website. "You" and
+            "your" refers to the individual to whom personal data covered by
+            this Privacy Policy relates.
+          </p>
+        </div>
+
+        <div className="flex flex-col  space-y-4 mt-6">
+          <h1 className="text-2xl font-medium">Information We Collect</h1>
+          <p className="text-[#475467] text-sm">
+            We collect personal data in the following ways:
+          </p>
+          <p className="font-bold">Information Provided Directly by You</p>
+          <ul className="text-left ">
+            <li>
+              ● Account registration information (name, email address, password)
+            </li>
+
+            <li>● Communication with WebMaxi</li>
+
+            <li>● Transaction details</li>
+
+            <li>● Payment information</li>
+            <li>● Identity verification information</li>
+            <li>● Public profile information</li>
+            <li>● Testimonials</li>
+          </ul>
+
+          <p className="font-bold">Information Received From Third Parties</p>
+          <ul className="text-left ">
+            <li>● Information from authorized resellers or distributors</li>
+
+            <li>
+              ● Information from other third parties (marketing partners,
+              publicly-available sources)
+            </li>
+          </ul>
+
+          <p className="font-bold">Information Collected Automatically</p>
+          <ul className="text-left ">
+            <li>● Device and usage data</li>
+
+            <li>● Cookies and similar technologies</li>
+          </ul>
+        </div>
+
+        <div className="flex flex-col  space-y-4 mt-6">
+          <h1 className="text-2xl font-medium">How We Use Your Information</h1>
+          <p className="text-[#475467]">We use your personal data to:</p>
+
+          <ul className="text-left ">
+            <li>● Provide and improve our Services</li>
+
+            <li>● Manage our relationship with you</li>
+
+            <li>
+              ● Analyze preferences and behavior to enhance user experience
+            </li>
+
+            <li>● Conduct internal business operations</li>
+            <li>● Prevent fraud and ensure security</li>
+            <li>● Comply with legal obligations</li>
+            <li>
+              ● For marketing and advertising purposes (with your consent where
+              required)
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex flex-col  space-y-4 mt-6">
+          <h1 className="text-2xl font-medium">
+            How We Disclose Your Personal Data
+          </h1>
+          <p className="text-[#475467] ">
+            WebMaxi will not sell, rent, lease or otherwise provide your
+            personal data to others, except:
+          </p>
+
+          <ul className="text-left ">
+            <li>● To provide you with the products and services you request</li>
+
+            <li>● To provide you with the products and services you request</li>
+
+            <li>● To affiliates for administrative purposes</li>
+
+            <li>● To resellers or distributors to facilitate sales</li>
+            <li>● In connection with business transfers</li>
+            <li>● To comply with legal requirements</li>
+          </ul>
+        </div>
+
+        <div className="flex flex-col  space-y-4 mt-6">
+          <h1 className="text-2xl font-medium">Data Retention</h1>
+          <p className="text-[#475467]">
+            We retain your personal data for as long as necessary to provide the
+            Services, comply with legal obligations, or for other legitimate
+            business purposes.
+          </p>
+        </div>
+
+        <div className="flex flex-col  space-y-4 mt-6">
+          <h1 className="text-2xl font-medium">
+            How We Protect Your Personal Data
+          </h1>
+          <p className="text-[#475467]">
+            We have implemented physical, technical, and administrative security
+            measures to protect your personal data. However, no method of
+            transmission over the Internet is 100% secure.
+          </p>
+        </div>
+
+        <div className="flex flex-col  space-y-4 mt-6">
+          <h1 className="text-2xl font-medium">
+            International Transfers of Personal Data
+          </h1>
+          <p className="text-[#475467]">
+            WebMaxi is based in the UK. We may transfer your personal data to
+            countries outside the UK or EEA. When we do, we ensure appropriate
+            safeguards are in place to protect your data
+          </p>
+        </div>
+
+        <div className="flex flex-col  space-y-4 mt-6">
+          <h1 className="text-2xl font-medium">Privacy Rights and Choices</h1>
+          <p className="text-[#475467] ">
+            Depending on your location, you may have rights to:
+          </p>
+
+          <ul className="text-left ">
+            <li>● Access your personal data</li>
+
+            <li>● Correct inaccurate data</li>
+
+            <li>● Delete your data</li>
+
+            <li>● Object to or restrict certain processing</li>
+            <li>● Data portability</li>
+            <li>● Withdraw consent (where applicable)</li>
+          </ul>
+        </div>
+
+        <div className="flex flex-col  space-y-4 mt-6">
+          <h1 className="text-2xl font-medium">Children</h1>
+          <p className="text-[#475467]">
+            Our Services are not directed to children under the age of 16. We do
+            not knowingly collect personal data from children under 16.
+          </p>
+        </div>
+
+        <div className="flex flex-col  space-y-4 mt-6">
+          <h1 className="text-2xl font-medium">Contact Information</h1>
+          <p className="text-[#475467]">
+            If you have any questions or concerns regarding this Privacy Policy
+            or use of personal data, please contact us at:
+          </p>
+          <p className="text-[#475467]">WebMaxi.</p>
+
+          <p className="text-[#475467]">Registered office address</p>
+
+          <p className="text-[#475467]">
+            133 Station Road, Sidcup, Kent, England, DA15 7AA
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
